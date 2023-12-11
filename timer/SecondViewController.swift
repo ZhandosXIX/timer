20021390c6e4e0145c2c671d0e6e3b4d88619b85
ZhandosXIX <ospanbekov.k@mail.ru>
2023-12-11 16:32:50 +0600

    first commit

--- timer/SecondViewController.swift
+++ timer/SecondViewController.swift
@@ -8,14 +8,65 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    var time = 0
+    
+    var startTime = 0
+    
+    var timer = Timer()
+    
+    var isTimerRunning = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        label.text = timeToString(IntTime: time)
+        
+        startTime = time
     }
     
+    @objc func countTime(){
+        time -= 1
+        label.text = timeToString(IntTime: time)
+        
+        if time == 0 {
+            timer.invalidate()
+            //isTimerRunning = false
+        }
 
+    }
+    
+    @IBAction func startTimer(_ sender: Any) {
+        if isTimerRunning {
+            return
+        }
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countTime), userInfo: nil, repeats: true)
+        
+        isTimerRunning = true
+    }
+    
+    @IBAction func pauseTimer(_ sender: Any) {
+        timer.invalidate()
+        isTimerRunning = false
+    }
+    
+    @IBAction func restartTimer(_ sender: Any) {
+        timer.invalidate()
+        time = startTime
+        isTimerRunning = false
+        label.text = timeToString(IntTime: time)
+
+    }
+    func timeToString(IntTime: Int) -> String{
+        let second = IntTime % 60
+        let minutes = (IntTime / 60) % 60
+        let hours = IntTime / 3600
+        
+        return String(format: "%0.2d:%0.2d:%0.2d", hours, minutes, second)
+    }
+    
     /*
     // MARK: - Navigation
 
--- timer/ViewController.swift
+++ timer/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "secondVC"{
+            if let secondVC = segue.destination as? SecondViewController{
+                secondVC.time = Int(TextField.text!) ?? 0
+            }
+        }
+    }
 
 }
 
